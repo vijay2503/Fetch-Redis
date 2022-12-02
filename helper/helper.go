1f0045ce86d4d2b5369df02b946d05d7ee971398
vijay2503 <s.vijaynivas@gmail.com>
2022-12-02 18:38:35 +0530

    first commit

--- helper/helper.go
+++ helper/helper.go
@@ -1,7 +1,32 @@
 package helper
 
-//Pattermatching deletion
-func Response() *map[string]interface{} {
-	response := make(map[string]interface{})
-	return &response
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Response(code, status string, resp interface{}) string {
+	var response = struct {
+		Code     string
+		Status   string
+		Response interface{}
+	}{Code: code, Status: status, Response: resp}
+	result, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	return string(result)
+}
+func ErrResponse(code, status string, resp interface{}) string {
+	var response = struct {
+		Code   string
+		Status string
+		Error  interface{}
+	}{Code: code, Status: status, Error: resp}
+	result, err := json.Marshal(response)
+	if err != nil {
+		return err.Error()
+	}
+	return string(result)
 }
--- router/middleware.go
+++ router/middleware.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	h "qwik/helper"
 	"qwik/model"
 	s "qwik/server"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -21,17 +23,20 @@ func CreateData(c echo.Context) error {
 	}
 	if err := LocalServe.Insert(create); err != nil {
 		if err.Error() == `pq: duplicate key value violates unique constraint "server_data_key_key"` {
-			return c.String(http.StatusBadRequest, "Already This key has Exist allow Only Updation")
+			return c.String(http.StatusBadRequest, h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+				"Failed", err.Error()))
 		}
 		fmt.Println(err)
-		return c.String(http.StatusBadRequest, "Failed to Insert Data to Postgres")
+		return c.String(http.StatusBadRequest, h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+			"Failed to Insert postgres", err.Error()))
 	}
 	_, err := LocalServe.SetRedis(create.Key, create.Value)
 	if err != nil {
-		return c.String(http.StatusBadRequest, "Failed to Store the Data in redis")
+		return c.String(http.StatusBadRequest, h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+			"Failed to Store the Data in redis", err.Error()))
 	}
-
-	return c.String(http.StatusOK, "Successfully Inserted\n"+"{"+create.Key+":"+create.Value+"}")
+	return c.String(http.StatusOK, h.Response(strconv.Itoa(http.StatusOK),
+		"Sucessfuly Inserted", "{"+create.Key+":"+create.Value+"}"))
 }
 
 //GetData handles the GET request from client
@@ -41,16 +46,23 @@ func GetData(c echo.Context) error {
 	if v == "" || err != nil {
 		rr, value := LocalServe.GET(key)
 		if rr != nil {
-			return c.String(http.StatusBadRequest, "Failed to retrieve data from Local Data-Base")
+			return c.String(http.StatusBadRequest,
+				h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+					"Failed to retrieve data from Local Data-Base", err.Error()))
+
 		} else {
 			_, er := LocalServe.SetRedis(key, value)
 			if er != nil {
-				return c.String(http.StatusBadRequest, "Failed to SeT data from Local Data-Base to Redis")
+				return c.String(http.StatusBadRequest,
+					h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+						"Failed to SeT data from Local Data-Base to Redis", err.Error()))
 			}
-			return c.String(http.StatusOK, "Retrieve from Local Data-Base-Server "+`{`+key+`:`+value+`}`)
+			return c.String(http.StatusOK, h.Response(strconv.Itoa(http.StatusOK),
+				"Retrieve from Local Data-Base-Server", `{`+key+`:`+value+`}`))
 		}
 	}
-	return c.String(http.StatusOK, "Retrieve from Redis-Server "+`{`+key+`:`+v+`}`)
+	return c.String(http.StatusOK, h.Response(strconv.Itoa(http.StatusOK),
+		"Retrieve from Redis-Server", `{`+key+`:`+v+`}`))
 }
 
 //DeleteData handles the DELETE request from client
@@ -59,12 +71,17 @@ func DeleteData(c echo.Context) error {
 	err := LocalServe.DelRedis(delKey)
 	if err != nil {
 		if err.Error() == errors.New("Key Does Not Exist").Error() {
-			return c.String(http.StatusBadRequest, "Deleted Key Does Not Exist")
+			return c.String(http.StatusBadRequest,
+				h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+					"Failed to Delete", err.Error()))
 		} else {
-			return c.String(http.StatusBadRequest, "Failed to Delete the data")
+			return c.String(http.StatusBadRequest,
+				h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+					"Failed to Delete", err.Error()))
 		}
 	}
-	return c.String(http.StatusOK, "Success fully deleted the data")
+	return c.String(http.StatusOK, h.Response(strconv.Itoa(http.StatusOK),
+		"Sucessfuly Deleted", "OK"))
 }
 
 //UpdateData handles the PUT request from client
@@ -74,15 +91,22 @@ func UpdateData(c echo.Context) error {
 	err := LocalServe.Update(updateKey, updateValue)
 	if err != nil {
 		if err.Error() == "record not found" {
-			return c.String(http.StatusBadRequest, "Updated Key Does Not Exist")
+			return c.String(http.StatusBadRequest,
+				h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+					"Failed", err.Error()))
 		} else {
-			return c.String(http.StatusBadRequest, "Failed to Update the data")
+			return c.String(http.StatusBadRequest,
+				h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+					"Failed to Update Data", err.Error()))
 		}
 	}
 
 	val, err := LocalServe.SetRedis(updateKey, updateValue)
 	if err != nil {
-		return c.String(http.StatusBadRequest, "Failed to Store the Data in redis")
+		return c.String(http.StatusBadRequest,
+			h.ErrResponse(strconv.Itoa(http.StatusBadRequest),
+				"Failed", err.Error()))
 	}
-	return c.String(http.StatusOK, val+"Success fully Updated the data\n"+`{`+updateKey+`:`+updateValue+`}`)
+	return c.String(http.StatusOK, h.Response(strconv.Itoa(http.StatusOK),
+		val, `{`+updateKey+`:`+updateValue+`}`))
 }
